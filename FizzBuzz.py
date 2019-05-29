d03b7834991ac20b45486042d76f8b8640561674
callummcguigan <49281580+callummcguigan@users.noreply.github.com>
2019-05-29 13:35:40 +0100

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,17 @@
+
+def fizzBuzz():
+
+    x = 1
+
+    for x in range(101):
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
+        else:
+            print(x)
+
+
+fizzBuzz()
